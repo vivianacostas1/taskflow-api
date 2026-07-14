e9ce82f79fff20a674a6765327f29a52386ac107
vivianacostas1 <vialcore123@hotmail.com>
2026-07-14 18:35:13 -0400

    chore: configurar CORS con FRONTEND_URL desde env

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const allowedOrigins = [
 ].filter(Boolean) as string[];
 
 app.use(cors({ 
-  origin: allowedOrigins, 
+  origin: '*', 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
